8d29e5f4a2497668ca0a34cf27a6098a05837b15
theoneandonlymacdolvenekeh <devmacdolven@gmail.com>
2020-09-16 14:32:24 -0400

    first commit

---
+++ CSProjects/comp1010/CProjects/hellloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char s;
+    printf(" What should i say helllo to ");
+    scanf("%d", &s);
+    printf("hello %d", s);
+
+    int mac, kevnen, max;     
+    return 0;
+}
